c77a5ce20595dadb819aea29482a06fd59ae53e8
rjulian <richard@rjulian.net>
2020-02-17 14:24:50 -0800

    Patch: pin version to engine code

--- detect_unencrypted_ebs_volume.tf
+++ detect_unencrypted_ebs_volume.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "detect_unencrypted_ebs_volume" {
-  source           = "git@github.com:cloudmitigator/reflex.git//modules/cwe_sns_email"
+  source           = "git@github.com:cloudmitigator/reflex.git//modules/cwe_sns_email?ref=v0.0.1"
   rule_name        = "DetectUnencryptedEBSVolume"
   rule_description = "Rule to check when EBS volumes are created without encryption."
 
