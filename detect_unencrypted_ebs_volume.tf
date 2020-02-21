f727112730a23ac0cc2ea9a31d093a1e39d91819
rjulian <richard@rjulian.net>
2020-02-21 13:52:44 -0800

    Initial commit

---
+++ detect_unencrypted_ebs_volume.tf
@@ -0,0 +1,35 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "detect_unencrypted_ebs_volume" {
+  source           = "git@github.com:cloudmitigator/reflex.git//modules/cwe_sns_email?ref=v0.0.1"
+  rule_name        = "DetectUnencryptedEBSVolume"
+  rule_description = "Rule to check when EBS volumes are created without encryption."
+
+  event_pattern = <<PATTERN
+{
+  "detail-type": [
+    "AWS API Call via CloudTrail"
+  ],
+  "source": [
+    "aws.ec2"
+  ],
+  "detail": {
+    "eventSource": [
+      "ec2.amazonaws.com"
+    ],
+    "eventName": [
+      "CreateVolume"
+    ],
+    "responseElements": {
+      "encrypted": [false]
+    }
+  }
+}
+PATTERN
+
+  topic_name = "DetectUnencryptedEBSVolume"
+  target_id  = "DetectUnencryptedEBSVolume"
+  email      = var.email
+}
