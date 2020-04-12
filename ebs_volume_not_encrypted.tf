1f0d56e74df81847a4e5647ee0402c21a6e9ce8e
Chris Coleman <chris@rot-26.com>
2020-04-12 16:08:56 -0600

    Renamed rule name: from `DetectUnencryptedEBSVolume` to `EbsVolumeNotEncrypted`

--- ebs_volume_not_encrypted.tf
+++ ebs_volume_not_encrypted.tf
@@ -1,6 +1,6 @@
 module "ebs_volume_not_encrypted" {
   source           = "git::https://github.com/cloudmitigator/reflex-engine.git//modules/cwe_lambda?ref=v0.5.7"
-  rule_name        = "DetectUnencryptedEBSVolume"
+  rule_name        = "EbsVolumeNotEncrypted"
   rule_description = "Rule to check when EBS volumes are created without encryption."
 
   event_pattern = <<PATTERN
@@ -25,7 +25,7 @@ module "ebs_volume_not_encrypted" {
 }
 PATTERN
 
-  function_name   = "DetectUnencryptedEBSVolume"
+  function_name   = "EbsVolumeNotEncrypted"
   source_code_dir = "${path.module}/source"
   handler         = "ebs_volume_not_encrypted.lambda_handler"
   lambda_runtime  = "python3.7"
@@ -33,10 +33,10 @@ PATTERN
     SNS_TOPIC = var.sns_topic_arn
   }
 
-  queue_name    = "DetectUnencryptedEBSVolume"
+  queue_name    = "EbsVolumeNotEncrypted"
   delay_seconds = 0
 
-  target_id  = "DetectUnencryptedEBSVolume"
+  target_id  = "EbsVolumeNotEncrypted"
 
   sns_topic_arn = var.sns_topic_arn
   sqs_kms_key_id = var.reflex_kms_key_id
